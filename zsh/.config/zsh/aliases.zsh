4e5d328e01c664444de5d225660699c0f0a2e337
Joshua Goodlett <joshuagoodlett@gmail.com>
2025-06-04 09:10:01 -0400

    Added git aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -19,3 +19,18 @@ alias rm='rm -i'
 alias nv='nvim'
 alias cat='bat'
 
+# Git shortcuts
+alias gc="git commit -m"
+alias gca="git commit -a -m"
+alias gp="git push origin HEAD"
+alias gpu="git pull origin"
+alias gs="git status"
+alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
+alias gd="git diff"
+alias gc="git checkout"
+alias gb='git branch'
+alias gba='git branch -a'
+alias ga='git add'
+alias gcoall='git checkout -- .'
+alias gr='git remote'
+alias gre='git reset'
