35ea7a37f09a56a70eba642daf8b1587b60b52e9
Joshua Goodlett <joshuagoodlett@gmail.com>
2025-06-03 20:20:58 -0400

    Update security

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -20,11 +20,3 @@ export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 
-# Configure pe3 to write to temp
-export APPDATA=/tmp
-
-# The local mongo host used to seed data.
-export MONGO_CLIENT_HOST=mongodb://host.docker.internal:27017/
-
-# the NPM token used to download node dependencies
-export NPM_TOKEN=6355a675-5f5e-4422-8be7-f7e57be3f615
